bb7f7965627812adcaabfd13dacbe9a3523586af
Damian Ebelties <ebelties@users.noreply.github.com>
2019-11-25 19:18:36 +0100

    Update corsy.py

--- corsy.py
+++ corsy.py
@@ -3,7 +3,7 @@
 
 import argparse
 
-from core.utils import load_json, host
+from core.utils import load_file, load_json, host
 from core.tests import active_tests
 from core.colors import white, green, info, bad, good, grey, end
 
@@ -17,39 +17,40 @@ print('''
 ''' % (green, white, grey, white, end))
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', help='target url', dest='url')
+parser.add_argument('-u', help='single url', dest='url')
+parser.add_argument('-l', help='list with urls', dest='list')
 parser.add_argument('-d', help='request delay', dest='delay', type=float, default=0)
 args = parser.parse_args()
 
+target_list = args.list
 target_url = args.url
 delay = args.delay
 
-def cors(target, delay, scheme=False):
-	url = target
-	if not target.startswith(('http://', 'https://')):
-		url = scheme + '://' + url
-	root = host(url)
-	parsed = urlparse(url)
-	netloc = parsed.netloc
-	scheme = parsed.scheme
-	url = scheme + '://' + netloc
-	active = active_tests(url, root, scheme, delay)
-	return active
+def cors(target):
+	if target.startswith(('http://', 'https://')):
+		parsed = urlparse(target)
+		root = host(target)
+		url = "%s://%s" % (parsed.scheme, parsed.netloc)
+		result = active_tests(url, root, parsed.scheme, delay)
+
+		if result:
+			print('%s Misconfiguration found!' % good)
+			print('%s URL: %s' % (info, target))
+			print('%s Title: %s' % (info, result))
+			print('%s Description: %s' % (info, details[result.lower()]['Description']))
+			print('%s Severity: %s' % (info, details[result.lower()]['Severity']))
+			print('%s Exploitation: %s\n' % (info, details[result.lower()]['Exploitation']))
+		else:
+			print('%s No misconfiguration found on %s.\n' % (bad, target))
+	else:
+		print('%s Please use https://%s not %s.\n' % (bad, target, target))
 
 details = load_json('./db/details.json')
 
 if target_url:
-	if target_url.startswith(('http://', 'https://')):
-		result = cors(target_url, delay)
-		if result:
-			print('%s Misconfiguration found!' % good)
-			print('%s Title: %s' % (info, result))
-			print('%s Description: %s' % (info, details[result.lower()]['Description']))
-			print('%s Severity: %s' % (info, details[result.lower()]['Severity']))
-			print('%s Exploitation: %s' % (info, details[result.lower()]['Exploitation']))
-		else:
-			print('%s No misconfiguration found.' % bad)
-	else:
-		print('%s Please use https://example.com not example.com' % bad)
+	cors(target_url)
+elif target_list:
+	for url in filter(None, load_file(target_list).split("\n")):
+		cors(url)
 else:
-	print('\n' + parser.format_help().lower())
+	print(parser.format_help().lower())
