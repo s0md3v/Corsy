ed7eb1768353fe1e72136b639ba5988bdd5d5c4b
Somdev Sangwan <s0md3v@gmail.com>
2019-12-13 18:13:21 +0530

    supress bad ssl warning

--- core/requester.py
+++ core/requester.py
@@ -1,4 +1,7 @@
+import urllib3
 import requests
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0',
